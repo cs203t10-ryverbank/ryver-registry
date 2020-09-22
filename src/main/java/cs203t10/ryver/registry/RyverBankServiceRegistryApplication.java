8c6c9861dde81aef543ce7752a18ab8adc3dd654
Bryan Lee <bryanmylee@gmail.com>
2020-09-22 23:52:41 +0800

    Initial commit

---
+++ src/main/java/cs203t10/ryver/registry/RyverBankServiceRegistryApplication.java
@@ -0,0 +1,11 @@
+package cs203t10.ryver.registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RyverBankServiceRegistryApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(RyverBankServiceRegistryApplication.class, args);
+	}
+}
---
+++ src/test/java/cs203t10/ryver/registry/RyverBankServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package cs203t10.ryver.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RyverBankServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
