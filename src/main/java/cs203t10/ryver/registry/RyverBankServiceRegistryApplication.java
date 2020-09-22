627e6dfdf85bc0b1fa26bf2f0838dbd2466700a7
Bryan Lee <bryanmylee@gmail.com>
2020-09-23 00:32:28 +0800

    Implement Eureka service server

--- src/main/java/cs203t10/ryver/registry/RyverBankServiceRegistryApplication.java
+++ src/main/java/cs203t10/ryver/registry/RyverBankServiceRegistryApplication.java
@@ -2,8 +2,10 @@ package cs203t10.ryver.registry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class RyverBankServiceRegistryApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RyverBankServiceRegistryApplication.class, args);
